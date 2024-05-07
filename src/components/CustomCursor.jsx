3b2e14cb2d9deb1ebc6b2a9eab290a8b8ebcbf20
benab04 <benabrahambiju@gmail.com>
2024-05-07 21:59:40 +0530

    hide cursor in mobile

--- src/components/CustomCursor.jsx
+++ src/components/CustomCursor.jsx
@@ -4,8 +4,9 @@ import { useCursor } from "./CursorContext";
 import "../styles/CustomCursor.css";
 
 const CustomCursor = () => {
-  const { isHovering } = useCursor();
+  const { isHovering, isInWindow, setInwindow } = useCursor();
   const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [isVisible, setIsVisible] = useState(true);
   const CircleColor = isHovering ? "rgba(54, 12, 205, 0.311)" : "transparent";
   const innerCircleDisplay = isHovering ? "none" : "block";
   useEffect(() => {
@@ -13,42 +14,57 @@ const CustomCursor = () => {
       setCursorPosition({ x: e.clientX, y: e.clientY });
     };
 
-    document.addEventListener("mousemove", moveCursor);
+    const handleMouseLeave = () => {
+      setIsVisible(false);
+    };
 
+    const handleMouseEnter = () => {
+      setIsVisible(true);
+    };
+
+    document.addEventListener("mousemove", moveCursor);
+    document.addEventListener("mouseleave", handleMouseLeave);
+    document.addEventListener("mouseenter", handleMouseEnter);
     return () => {
       document.removeEventListener("mousemove", moveCursor);
+      document.removeEventListener("mouseleave", handleMouseLeave);
+      document.removeEventListener("mouseenter", handleMouseEnter);
     };
-  }, []);
+  }, [isHovering]);
 
   return (
-    <div
-      className="custom-cursor"
-      style={{ left: cursorPosition.x, top: cursorPosition.y }}
-    >
-      <div className="arrow"></div>
-      <div
-        className="inner-circle"
-        style={{
-          width: 1,
-          height: 1,
-          left: cursorPosition.x,
-          top: cursorPosition.y,
-          display: innerCircleDisplay,
-        }}
-      ></div>
-      <div
-        className="circle"
-        style={{
-          width: isHovering ? "40px" : "25px",
-          height: isHovering ? "40px" : "25px",
+    <>
+      {isVisible && (
+        <div
+          className="custom-cursor"
+          style={{ left: cursorPosition.x, top: cursorPosition.y }}
+        >
+          <div className="arrow"></div>
+          <div
+            className="inner-circle"
+            style={{
+              width: 1,
+              height: 1,
+              left: cursorPosition.x,
+              top: cursorPosition.y,
+              display: innerCircleDisplay,
+            }}
+          ></div>
+          <div
+            className="circle"
+            style={{
+              width: isHovering ? "40px" : "25px",
+              height: isHovering ? "40px" : "25px",
 
-          left: isHovering ? cursorPosition.x - 15 : cursorPosition.x - 11,
-          top: isHovering ? cursorPosition.y - 15 : cursorPosition.y - 11,
+              left: isHovering ? cursorPosition.x - 15 : cursorPosition.x - 11,
+              top: isHovering ? cursorPosition.y - 15 : cursorPosition.y - 11,
 
-          backgroundColor: CircleColor,
-        }}
-      ></div>
-    </div>
+              backgroundColor: CircleColor,
+            }}
+          ></div>
+        </div>
+      )}
+    </>
   );
 };
 
